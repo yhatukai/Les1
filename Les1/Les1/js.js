e8e7378432e9d3ea2ef05de8492493dbb2b875a9
yhatukai <yhatukai@gmail.com>
2025-03-26 13:20:35 +0200

    Add files via upload

---
+++ Les1/Les1/js.js
@@ -0,0 +1,18 @@
+"use strict"
+// console.log("Yossef Hatukai");
+// const namesArr = ["Robert","Ronaldo","Dan"];
+// console.log(namesArr);
+
+
+// let name = prompt('What is your name: ');
+// console.log(name);
+
+//  let isDelete = confirm("Are you sure you want to delete?");
+
+//  if (isDelete)
+//   console.log("Deleted")
+
+let admin, name1;
+name1 = "John";
+admin = name1;
+console.log(admin)
